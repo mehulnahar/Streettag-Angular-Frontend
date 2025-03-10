e152eb25c6b19a2dd5e65bbb6c61e625b68e77cf
ZuberMindCrew <mohammed.zuber@mindcrewtech.com>
2025-03-10 12:30:08 +0530

    updated code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://52.56.93.181:3000/api/admin/',  // Update this with your actual API base URL
-  userUrl: 'http://52.56.93.181:3000/api/',
+  baseUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/admin/',  // Update this with your actual API base URL
+  userUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/',
   googleMapsApiKey: 'AIzaSyB9stNP2UYOkJCJkR2CfnabPiNP6g08UH8'
 //Clone
   //  baseUrl :'http://streettag-env-linux2-clone.eu-west-2.elasticbeanstalk.com/api/admin/',
