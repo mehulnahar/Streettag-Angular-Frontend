aea8c4b5e34df50a8f4a64b56f50c02003d2a127
ZuberMindCrew <mohammed.zuber@mindcrewtech.com>
2025-03-11 11:17:35 +0530

    update

--- src/app/pages/dashboard/disk-space/disk-space.component.ts
+++ src/app/pages/dashboard/disk-space/disk-space.component.ts
@@ -32,6 +32,8 @@ export class DiskSpaceComponent implements OnInit {
   public dailyMiles: number = 0;
   public weeklyMiles: number = 0;
   public monthlyMiles: number = 0;
+  public selectedLocation: number | null = null;
+  public selectedTeam: number | null = null;
   private readonly baseUrl = environment.baseUrl;
 
   constructor(
@@ -49,33 +51,52 @@ export class DiskSpaceComponent implements OnInit {
     });
   }
 
-  get_location_id(res: number): void {
+  get_location_id(locationId: number): void {
+    // Reset team and miles data when location changes
+    this.selectedLocation = locationId;
+    this.selectedTeam = null;
+    this.resetMilesData();
+    
     const url = `${this.baseUrl}getTeamByLocation`;
     const data1 = {
-      location_id: res,
+      location_id: locationId,
     };
     this.ajaxService.post(data1, url).subscribe((data: any) => {
       this.dataSourceTeam = data['response'];
     });
   }
 
-  get_team_id(res: number): void {
+  get_team_id(teamId: number): void {
+    this.selectedTeam = teamId;
     const url = `${this.baseUrl}getDataByTeam`;
     const data1 = {
-      team_id: res,
+      team_id: teamId,
     };
     this.ajaxService.post(data1, url).subscribe((data: any) => {
       this.dataSourceTeamDataAnalysis = data['response'];
-      if (this.dataSourceTeamDataAnalysis[0]?.[0]?.sum1) {
-        this.dailyMiles = this.dataSourceTeamDataAnalysis[0][0].sum1;
-      }
-      if (this.dataSourceTeamDataAnalysis[1]?.[0]?.sum7) {
-        this.weeklyMiles = this.dataSourceTeamDataAnalysis[1][0].sum7;
-      }
-      if (this.dataSourceTeamDataAnalysis[2]?.[0]?.sum30) {
-        this.monthlyMiles = this.dataSourceTeamDataAnalysis[2][0].sum30;
-      }
+      this.updateMilesData();
     });
+  }
+
+  private resetMilesData(): void {
+    this.dailyMiles = 0;
+    this.weeklyMiles = 0;
+    this.monthlyMiles = 0;
+  }
+
+  private updateMilesData(): void {
+    // Update daily miles
+    if (this.dataSourceTeamDataAnalysis[0]?.[0]?.sum1) {
+      this.dailyMiles = this.dataSourceTeamDataAnalysis[0][0].sum1;
+    }
+    // Update weekly miles
+    if (this.dataSourceTeamDataAnalysis[1]?.[0]?.sum7) {
+      this.weeklyMiles = this.dataSourceTeamDataAnalysis[1][0].sum7;
+    }
+    // Update monthly miles
+    if (this.dataSourceTeamDataAnalysis[2]?.[0]?.sum30) {
+      this.monthlyMiles = this.dataSourceTeamDataAnalysis[2][0].sum30;
+    }
   }
 
   ngOnInit(): void {}
