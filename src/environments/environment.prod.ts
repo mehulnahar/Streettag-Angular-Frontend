731a816480dae22a026695ac9cdefc0418206144
ZuberMindCrew <mohammed.zuber@mindcrewtech.com>
2025-03-17 11:44:54 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/admin/',  // Update this with your actual API base URL
-  userUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/',
+  baseUrl: 'https://wearestreettag.co.uk/api/admin/',  // Update this with your actual API base URL
+  userUrl: 'https://wearestreettag.co.uk/api/',
   googleMapsApiKey: 'AIzaSyB9stNP2UYOkJCJkR2CfnabPiNP6g08UH8'
 //Clone
   //  baseUrl :'http://streettag-env-linux2-clone.eu-west-2.elasticbeanstalk.com/api/admin/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/admin/',  // Update this with your actual API base URL
-  userUrl: 'http://streettag-env-24-clone.phq3pps4xm.eu-west-2.elasticbeanstalk.com/api/',
+  baseUrl: 'https://wearestreettag.co.uk/api/admin/',  // Update this with your actual API base URL
+  userUrl: 'https://wearestreettag.co.uk/api/',
   googleMapsApiKey: 'AIzaSyB9stNP2UYOkJCJkR2CfnabPiNP6g08UH8'
 };
 
